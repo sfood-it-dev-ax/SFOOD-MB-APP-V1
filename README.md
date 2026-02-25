6041e7eb65f199f6a09292383a6c04600f0807f5
donghyunlee-sfood <sfood.dhlee@gmail.com>
2026-02-25 09:21:59 +0900

    initail commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SFOOD-MB-APP-V1
+메모앱 API 서비스
+AI 기반 개발 테스트 버전 1 
+
---
+++ src/main/java/com/sfood/mb/app/SfoodMbAppV1Application.java
@@ -0,0 +1,13 @@
+package com.sfood.mb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfoodMbAppV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfoodMbAppV1Application.class, args);
+    }
+
+}
